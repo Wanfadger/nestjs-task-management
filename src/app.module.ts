84d25a2a5a7288c8e9f0e2305915b9e4c22dfe8d
wanfadger <galiwango.fahad@gmail.com>
2021-09-22 23:23:55 +0300

    setup database connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule , TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'wanfadgerdb',
+    password: 'password',
+    database: 'task',
+    synchronize: true,
+    autoLoadEntities:true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
