f7f1a7aacd92a498c4f8e046d001072e42bc228e
wanfadger <galiwango.fahad@gmail.com>
2021-09-24 09:38:21 +0300

    configured passportjs and jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
 
 @Module({
-    imports:[TypeOrmModule.forFeature([UsersRepository])],
+    imports:[TypeOrmModule.forFeature([UsersRepository]),
+    PassportModule.register({defaultStrategy:"jwt"}),
+    JwtModule.register({secret:"wanfadger" ,  signOptions:{
+      expiresIn:3600,
+      issuer:"wanfadger-task-management-api",
+       
+    }})
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
